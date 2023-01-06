40ca7a13ad9c82c50f3ea3d99740ba62b249c978
Adam Derylo <a.m.derylo@gmail.com>
2023-01-06 13:33:22 +0100

    Add mininimalistic UI

---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum softwareId {
+  display = 2,
+  add = 3,
+  modify = 4,
+} softwareId;
+
+void readClientName(char *ptr) {
+  // lol
+}
+
+void invokeSoftware(softwareId id, char *clientName) {}
+
+int main() {
+  int option = 0;
+  char *clientName = NULL;
+
+  while (option != 5) {
+    printf(
+        "What we want to do today? Type number corrsponding to the option.\n \
+            (1) Specify the client for whom operations will be performed.\n \
+            (2) Deposit and credit display software.\n \
+            (3) Software for adding deposits and loans.\n \
+            (4) Software for modifying the content of deposits and loans.\n \
+            (5) Quit.\n");
+
+    scanf("%d", &option);
+
+    if (option == 1) {
+      readClientName(clientName);
+    } else if (option > 1 && option < 5) {
+      if (clientName)
+        invokeSoftware(option, clientName);
+      else
+        printf("First you must specify client name, use option (1)\n");
+    } else if (option == 5) {
+      // clear memory
+      printf("Have a nice day!\n");
+    }
+  }
+
+  return 0;
+}
