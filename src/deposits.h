9d60f185f4607bb573c2ad3d36658920e05162fa
Adam Derylo <a.m.derylo@gmail.com>
2023-01-06 16:04:28 +0100

    Redo deposits header to comply with std from moodle

--- src/deposits.h
+++ src/deposits.h
@@ -10,38 +10,43 @@
 
 typedef struct account {
   char *name;
-  int idNumber;
+  int number;
+  char *date;
+  vector *subAccounts;
+};
+
+typedef struct subAccount {
+  int sum;
   vector *records;
 };
 
 typedef struct record {
-  int sum;
-  int percent;
+  int interest;
   int duration;
 };
 
 
-account *initAccount(int idNumber, char *name) {
+account *initAccount(int number, char *name) {
   account *acc = MALLOCATE(account);
-  acc->idNumber = idNumber;
+  acc->number = number;
   acc->name = MALLOCATE_ARRAY(char, strlen(name));
   strcpy(acc->name, name);
   return acc;
 }
 
-record *initRecord(int sum, int percent, int duration) {
-  record *rec = MALLOCATE(record);
-  rec->sum = sum;
-  rec->percent = percent;
-  rec->duration = duration;
-  return rec;
-}
+// record *initRecord(int sum, int percent, int duration) {
+//   record *rec = MALLOCATE(record);
+//   rec->sum = sum;
+//   rec->percent = percent;
+//   rec->duration = duration;
+//   return rec;
+// }
 
-bool addRecord(account *acc, record rec) {
-  record *recPtr = MALLOCATE(record);
-  *recPtr = rec;
-  return (vectorPushBack(acc->records, recPtr) == SUCCESS);
-}
+// bool addRecord(account *acc, record rec) {
+//   record *recPtr = MALLOCATE(record);
+//   *recPtr = rec;
+//   return (vectorPushBack(acc->records, recPtr) == SUCCESS);
+// }
 
 bool saveToFile(account *acc, char *filepath) {
   FILE *fptr;
@@ -51,13 +56,18 @@ bool saveToFile(account *acc, char *filepath) {
   }
 
   fprintf(fptr, "Name: %s\n", acc->name);
-  fprintf(fptr, "Id: %d", acc->idNumber);
-  for (size_t i = 0; i < vectorSize(acc->records); i++) {
-    record *rec = (record *)vectorGet(acc->records, i);
+  fprintf(fptr, "Number: %d\n", acc->number);
+  fprintf(fptr, "Date: %s\n", acc->date);
 
-    fprintf(fptr, "Sum: %d", rec->sum);
-    fprintf(fptr, "Percent: %d", rec->percent);
-    fprintf(fptr, "Duration: %d", rec->duration);
+  for (size_t i = 0; i < vectorSize(acc->subAccounts); i++) {
+    subAccount *subAcc = (subAccount *)vectorGet(acc->subAccounts, i);
+    fprintf(fptr, "Sum: %d\n", subAcc->sum);
+
+    for (size_t j = 0; j < vectorSize(subAcc->records); j++) {
+      record *rec = (record *)vectorGet(subAcc->records, j);
+      fprintf(fptr, "Procent: %d\n", rec->interest);
+      fprintf(fptr, "Duration: %d\n", rec->duration);
+    }
   }
 
 
