126af962373eda43ba89cb8ba4e12f4612aa8211
Adam Derylo <a.m.derylo@gmail.com>
2023-01-27 19:50:19 +0100

    Add firewall script

---
+++ app/firewall.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+sudo nft add table inet filter
+sudo nft add chain inet filter input { type filter hook input priority 0 \; }
+sudo nft add rule inet filter input tcp dport { 22, 80, 443 } accept
+sudo  add rule inet filter input icmp type echo-request accept
+sudo nft add rule inet filter input drop
