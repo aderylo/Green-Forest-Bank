1531dfe07a47871eb9f10603c71d842a32df42f4
Adam Derylo <a.m.derylo@gmail.com>
2023-02-20 20:10:12 +0100

    fix typos

--- app/setup.sh
+++ app/setup.sh
@@ -17,10 +17,10 @@ echo $text1 > credits/1.txt
 
 echo $text2 > credits/2.txt
 
-# set onwership
+# set ownership
 chown root credits/1.txt
 chown test credits/2.txt
 
-# restric read access to owner
+# restrict read access to owner
 chmod 600 credits/1.txt
 chmod 600 credits/2.txt
